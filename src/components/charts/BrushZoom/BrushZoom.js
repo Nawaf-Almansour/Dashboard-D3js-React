1e67e755adb7c1b1bcf1225bccb262cbfdb3ee1d
Nawaf Almansour <nawaaf.almansour@gmail.com>
2021-09-01 15:08:12 +0300

    add BrushZoom

---
+++ src/components/charts/BrushZoom/BrushZoom.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import {
+    VictoryChart,
+    VictoryZoomContainer,
+    VictoryScatter,
+} from "victory";
+
+
+function random(max, min) {
+    return Math.random() * (max - min) + min;
+
+}
+
+function range(size) {
+    return [...Array(size).keys()].map(i => i );
+
+}
+
+class BrushZoom extends React.Component {
+
+    constructor(props) {
+        super(props);
+    }
+
+    state = {
+        data: this.getScatterData()
+    }
+
+    getScatterData() {
+        return range(50).map((index) => {
+            return {
+                x: random(1, 50),
+                y: random(10, 90),
+                size: random(8) + 3
+            };
+        });
+    }
+
+    render() {
+        return (
+            <VictoryChart
+                domain={{y: [0, 100]}}
+                containerComponent={<VictoryZoomContainer zoomDomain={{x: [5, 35], y: [0, 100]}}/>}
+            >
+                <VictoryScatter
+                    data={this.state.data}
+                    style={{
+                        data: {
+                            opacity: ({ datum }) =>  datum.y % 5 === 0 ? 1 : 0.7,
+                            fill: ({ datum }) => datum.y % 5 === 0 ? "tomato" : "black"
+                        }
+                    }}
+                />
+            </VictoryChart>
+        );
+    }
+}
+
+export  default BrushZoom;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,6 +4,9 @@ import RectangleChart from "../components/shared/RectangleChart";
 import LineChart from "../components/shared/LineChart";
 import Scatterplot from "../components/shared/Scatterplot";
 import Chart from "../components/charts/LineChart/Chart/Chart";
+import SynchronizedLineCharts from "../components/SynchronizedLineCharts/SynchronizedLineCharts";
+import GroupedBar from "../components/charts/GroupedBar/GroupedBar";
+import BrushZoom from "../components/charts/BrushZoom/BrushZoom";
 
 
 class Home extends React.Component {
@@ -20,7 +23,11 @@ class Home extends React.Component {
           <LineChart/>
           <Scatterplot/>
                 <Chart />
+                <SynchronizedLineCharts/>
+                <GroupedBar/>
+                <BrushZoom/>
             </BaseLayout>
+
         )
     }
 
